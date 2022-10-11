a11b47a41976ca40212965b10dbc937fa65029a9
Takashi Uesaka <takashi.uesaka@microsoft.com>
2022-10-11 13:59:46 +0900

    create stating branch

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => {
 
    return (
       <>
-         <div>Hello!</div>
+         <div>Hello! Staging!</div>
          <React.Fragment>
             {users.map((val, index) => {
                return (
